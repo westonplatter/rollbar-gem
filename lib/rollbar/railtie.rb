ea888e36e67c55f42e4b3e8346daac0f3c045b54
Jon de Andres <jon@rollbar.com>
2015-09-28 23:12:37 +0200

    Remove rollbar/rails.rb

--- lib/rollbar/railtie.rb
+++ lib/rollbar/railtie.rb
@@ -1,30 +1,34 @@
-require 'rails/railtie'
 require 'rollbar'
+require 'rails/railtie'
 
 module Rollbar
   class Railtie < ::Rails::Railtie
     rake_tasks do
       require 'rollbar/rake_tasks'
     end
-    
-    initializer 'rollbar.middleware.rails' do |app|
-      if defined?(ActionDispatch::DebugExceptions)
-        # Rails 3.2.x+
-        require 'rollbar/middleware/rails/rollbar'
-        require 'rollbar/middleware/rails/show_exceptions'
-        
-        app.config.middleware.insert_after ActionDispatch::DebugExceptions,
-          Rollbar::Middleware::Rails::RollbarMiddleware
-        ActionDispatch::DebugExceptions.send(:include, Rollbar::Middleware::Rails::ShowExceptions)
-      elsif defined?(ActionDispatch::ShowExceptions)
-        # Rails 3.0.x and 3.1.x
-        require 'rollbar/middleware/rails/rollbar'
-        require 'rollbar/middleware/rails/show_exceptions'
-        
-        app.config.middleware.insert_after ActionDispatch::ShowExceptions,
-          Rollbar::Middleware::Rails::RollbarMiddleware
-        ActionDispatch::ShowExceptions.send(:include, Rollbar::Middleware::Rails::ShowExceptions)
-      end
+
+    initializer 'rollbar.middleware.rails.30_and_31' do |app|
+      return unless defined?(ActionDispatch::ShowExceptions)
+
+      # Rails 3.0.x and 3.1.x
+      require 'rollbar/middleware/rails/rollbar'
+      require 'rollbar/middleware/rails/show_exceptions'
+
+      app.config.middleware.insert_after ActionDispatch::ShowExceptions,
+                                         Rollbar::Middleware::Rails::RollbarMiddleware
+      ActionDispatch::ShowExceptions.send(:include, Rollbar::Middleware::Rails::ShowExceptions)
+    end
+
+    initializer 'rollbar.middleware.rails32+' do |app|
+      return unless defined?(ActionDispatch::DebugExceptions)
+
+      # Rails 3.2.x+
+      require 'rollbar/middleware/rails/rollbar'
+      require 'rollbar/middleware/rails/show_exceptions'
+
+      app.config.middleware.insert_after ActionDispatch::DebugExceptions,
+                                         Rollbar::Middleware::Rails::RollbarMiddleware
+      ActionDispatch::DebugExceptions.send(:include, Rollbar::Middleware::Rails::ShowExceptions)
     end
 
     config.after_initialize do
